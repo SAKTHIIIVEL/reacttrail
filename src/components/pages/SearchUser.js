f0d0ef0f078d084ac43e3e2b5f66cdbcafec9976
SAKTHIIIVEL <sssakthivel928@gmail.com>
2025-01-30 16:17:07 +0530

    search feature added

--- src/components/pages/SearchUser.js
+++ src/components/pages/SearchUser.js
@@ -1,42 +1,92 @@
-import { TextField } from '@mui/material';
-import React from 'react'
+import React, { useEffect } from 'react'
+import { TextField, Typography, CircularProgress,Card, CardMedia, CardContent } from "@mui/material";
+import Grid from '@mui/material/Grid';
 
 
-function SearchUser() {
-    const [news,setNews] = React.useState([]);
-    let [searchText,setSearchText] = React.useState("");
-    let [loading, setLoading] = React.useState(false);
 
-    const searchUser = async()=> {
-    
-        const NEWS_API = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=ba772a5854384ee3908a792d34ce26f9"
-        const res =  await fetch(NEWS_API); 
-        const data= await res.json();
-        setNews(data.articles);
+export const SearchUser = () => {
+  const [news, setNews] = React.useState([]);
+  let [searchText, setSearchText] = React.useState("");
+  let [loading, setLoading] = React.useState(true);
+  const [filteredNews,setfilteredNews] = React.useState([]);
+
+  useEffect(() => {
+      
+    const fetchNews = async()=>{
+        try {
+            const NEW_API = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=15b70fa011a94eef990296ebf4609a11";
+            
+            const res = await fetch(NEW_API);
+            const data = await res.json();
+            setNews(data.articles);
+            setfilteredNews(data.articles);
+            setLoading(false)
+            
+        } catch (error) {
+            console.log("No News Found, Error Message",error);    
+        }finally{
+            setLoading(false);
         }
+  }
+  fetchNews()
+  }, []);
+
+  const handleChange = (event) => {
+    const term = event.target.value;
+    setSearchText(term);
+
+    if(term.trim() === ''){
+      setfilteredNews(news)
+    }else{
+      const filtered = news.filter((item)=> item.title.toLowerCase().includes(term.toLowerCase()));
+      console.log(filtered);     
+      setfilteredNews(filtered);
+    }
+
+  }
 
   return (
     <div>
-        <h3>SearchUser</h3>
-        <TextField
-        fullWidth
-        label="Search News By Title"
-        value={searchText}
-        onChange={handleChange}
-        margin='normal'
-        />
-        
-        <button onClick={searchUser}>Search</button>
-        {
-            news.map((item)=>{
-                return(
-                    <div>{item.title}</div>
-                );
-            })
-        }
+    <h3>SearchUser</h3>
+    <TextField
+      fullWidth
+      label="Search News by Title"
+      value={searchText}
+      onChange={handleChange}
+      margin='normal'
+    />
 
+    
+    {
+      loading ?(
+        <div>
+          <CircularProgress/>
+
+        </div>
+      ) : (
+        <div>
+          <Grid container rowSpacing={1} columnSpacing={{xs:1,sm:3,md:4}}>
+          {
+            filteredNews.map((value, index)=>{
+              return(
+                  <Grid item key={index} xs={6} md={4} >
+                        <Card>
+                            <CardMedia sx={{height:'200px'}} image={value.urlToImage} />
+                            <CardContent>
+                                <Typography variant ='h5'>{value.title}</Typography>
+                                <Typography variant='body2'>{value.description}</Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+              )
+            })
+          }
+          </Grid>
+        </div>
+      )
+    }
     </div>
   )
 }
 
-export default SearchUser
+export default SearchUser
